286d43bf884ae7dc6050b55db97ffef725bbd6e0
TamerDotWork <74485569+TamerDotWork@users.noreply.github.com>
2025-11-27 16:47:46 +0400

    js

--- static/js/script.js
+++ static/js/script.js
@@ -49,3 +49,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
             }
         });
 
+ // Trigger the hidden file input when the button is clicked
+        function openFileOption() {
+            document.getElementById("fileInput").click();
+        }
